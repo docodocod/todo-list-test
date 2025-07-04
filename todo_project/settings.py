aaada2e2af9544ec5caef81c0dec550f146b8fb4
dongan <dongan0618@naver.com>
2025-07-04 21:12:25 +0900

    배포 수정

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -19,8 +19,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "todo",
-    'rest_framework',
-    'corsheaders',  
 ]
 
 MIDDLEWARE = [
@@ -31,7 +29,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
